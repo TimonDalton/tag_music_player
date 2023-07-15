963730253ce61b031063364e0018a5b5f707172b
Timon D <Timondalton2001@gmail.com>
2023-07-15 20:05:17 +0200

    Added playback bar functionality

--- lib/timoncode/widgets/common/playback_bar.dart
+++ lib/timoncode/widgets/common/playback_bar.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:tag_music_player/timoncode/models/song.dart';
+import 'package:tag_music_player/timoncode/control_spotify/playback.dart';
 
 class PlaybackBar extends StatefulWidget {
   PlaybackBar({
@@ -19,10 +20,8 @@ class PlaybackBar extends StatefulWidget {
 }
 
 class _PlaybackBarState extends State<PlaybackBar> {
-
   @override
   Widget build(BuildContext context) {
-
     return Container(
       width: double.infinity,
       height: 80.0,
@@ -37,126 +36,123 @@ class _PlaybackBarState extends State<PlaybackBar> {
           decoration: BoxDecoration(
             color: FlutterFlowTheme.of(context).accent1,
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Align(
-                  alignment: AlignmentDirectional(0.0, -1.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Align(
-                          alignment: AlignmentDirectional(0.0, 0.0),
-                          child: Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(
-                                25.0, 0.0, 0.0, 0.0),
-                            child: Container(
-                              width: double.infinity,
-                              height: 40.0,
-                              decoration: BoxDecoration(
-                                color: Color(0x00FFFFFF),
-                              ),
-                              child: Align(
-                                alignment: AlignmentDirectional(0.0, 0.0),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    Align(
-                                      alignment:
-                                          AlignmentDirectional(-1.0, 0.0),
-                                      child: Text(
-                                        widget.song.name,
-                                        style: FlutterFlowTheme.of(context)
-                                            .titleLarge
-                                            .override(
-                                              fontFamily: 'Roboto Condensed',
-                                              fontSize: 16.0,
-                                            ),
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment:
-                                          AlignmentDirectional(-1.0, 0.0),
-                                      child: Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 3.0, 0.0, 0.0),
-                                        child: Text(
-                                          widget.song.artist,
-                                          style: FlutterFlowTheme.of(context)
-                                              .bodyMedium
-                                              .override(
-                                                fontFamily: 'Roboto Condensed',
-                                                fontSize: 12.0,
-                                              ),
+          child: StreamBuilder(
+            builder: (context, snapshot) {
+              PlayerState playerState = snapshot.data as PlayerState;
+              String playbackPositionText = playerState.playbackPosition.toString();
+              double progress = playerState.playbackPosition.toDouble() / widget.song.duration.toDouble();
+              return Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Align(
+                      alignment: AlignmentDirectional(0.0, -1.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Align(
+                              alignment: AlignmentDirectional(0.0, 0.0),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(25.0, 0.0, 0.0, 0.0),
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 40.0,
+                                  decoration: BoxDecoration(
+                                    color: Color(0x00FFFFFF),
+                                  ),
+                                  child: Align(
+                                    alignment: AlignmentDirectional(0.0, 0.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Align(
+                                          alignment: AlignmentDirectional(-1.0, 0.0),
+                                          child: Text(
+                                            widget.song.name,
+                                            style: FlutterFlowTheme.of(context).titleLarge.override(
+                                                  fontFamily: 'Roboto Condensed',
+                                                  fontSize: 16.0,
+                                                ),
+                                          ),
                                         ),
-                                      ),
+                                        Align(
+                                          alignment: AlignmentDirectional(-1.0, 0.0),
+                                          child: Padding(
+                                            padding: EdgeInsetsDirectional.fromSTEB(0.0, 3.0, 0.0, 0.0),
+                                            child: Text(
+                                              widget.song.artist,
+                                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                    fontFamily: 'Roboto Condensed',
+                                                    fontSize: 12.0,
+                                                  ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ],
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
+                          Expanded(
+                            child: InkWell(
+                              onTap: () async{
+                                if(playerState.isPaused){
+                                  await resume();
+                                }else{
+                                  await pause();
+                                }
+                              },
+                              child: Icon(
+                                playerState.isPaused ? Icons.play_arrow_rounded : Icons.pause,
+                                color: FlutterFlowTheme.of(context).primaryText,
+                                size: 50.0,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        child: Icon(
-                          Icons.play_arrow_rounded,
-                          color: FlutterFlowTheme.of(context).primaryText,
-                          size: 50.0,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: AlignmentDirectional(0.0, 1.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Padding(
-                        padding:
-                            EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 0.0, 0.0),
-                        child: 
-                        StreamBuilder(builder: (context, snapshot) {
-                          PlayerState playerState = snapshot.data as PlayerState;
-                          String text = playerState.playbackPosition.toString();
-                          return Text(
-                          text,
-                          style: FlutterFlowTheme.of(context).bodyMedium,
-                        );
-                        },)
-                        
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional(0.0, 1.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 0.0, 0.0),
+                            child: Text(
+                              playbackPositionText,
+                              style: FlutterFlowTheme.of(context).bodyMedium,
+                            ),
+                          ),
+                          Expanded(
+                            child: LinearProgressIndicator(
+                              value: progress,
+                              backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+                              minHeight: 4,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 5.0, 0.0),
+                            child: Text(
+                              widget.song.duration.toString(),
+                              style: FlutterFlowTheme.of(context).bodyMedium,
+                            ),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        child: StreamBuilder(builder: (context, snapshot) {
-                          PlayerState playerState = snapshot.data as PlayerState;
-                          double progress = playerState.playbackPosition.toDouble()/widget.song.duration.toDouble();
-                          return LinearProgressIndicator(
-                            value: progress,
-                            backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
-                            minHeight: 4,
-                            color: Colors.white70,
-                          );
-                        },),),
-                      Padding(
-                        padding:
-                            EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 5.0, 0.0),
-                        child: Text(
-                          widget.song.duration.toString(),
-                          style: FlutterFlowTheme.of(context).bodyMedium,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-            ],
+                ],
+              );
+            },
           ),
         ),
       ),
